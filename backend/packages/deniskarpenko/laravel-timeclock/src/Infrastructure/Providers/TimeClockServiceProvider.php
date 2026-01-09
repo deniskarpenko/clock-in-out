ccf8f02a34fe6bf543f430c5990cb4bd5fed6c1d
Denys Karpenko <denisdataforsep@gmail.com>
2026-01-08 19:25:41 -0800

    develop:add provider

--- backend/packages/deniskarpenko/laravel-timeclock/src/Infrastructure/Providers/TimeClockServiceProvider.php
+++ backend/packages/deniskarpenko/laravel-timeclock/src/Infrastructure/Providers/TimeClockServiceProvider.php
@@ -5,5 +5,18 @@ declare(strict_types=1);
 namespace Deniskarpenko\Timeclock\Infrastructure\Providers;
 
 use Illuminate;
+use  Illuminate\Support\ServiceProvider;
 
-class TimeClockServiceProvider extends Illuminate\Support\ServiceProvider {}
+class TimeClockServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        // Загрузка миграций из пакета
+        $this->loadMigrationsFrom(__DIR__ . '/../../../database/migrations');
+
+        // Публикация миграций (опционально)
+        $this->publishes([
+            __DIR__ . '/../../../database/migrations' => database_path('migrations'),
+        ], 'time-tracking-migrations');
+    }
+}
